d94e4c27a6312ee83797235a36ef9166e315981d
Tushar0744 <f20191065@g0a.bits-pilani.ac.in>
2024-06-11 12:31:19 +0530

    Changes in environment variables

--- src/main/java/com/identity_reconciliation/identity_reconciliation/config/DataSourceConfig.java
+++ src/main/java/com/identity_reconciliation/identity_reconciliation/config/DataSourceConfig.java
@@ -1,18 +1,18 @@
-package com.identity_reconciliation.identity_reconciliation.config;
-
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class DataSourceConfig {
-  @Bean
-  @ConfigurationProperties(prefix = "spring.datasource")
-  public DataSource dataSource() {
-    return DataSourceBuilder.create().type(HikariDataSource.class).build();
-  }
-}
+//package com.identity_reconciliation.identity_reconciliation.config;
+//
+//import com.zaxxer.hikari.HikariDataSource;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class DataSourceConfig {
+//  @Bean
+//  @ConfigurationProperties(prefix = "spring.datasource")
+//  public DataSource dataSource() {
+//    return DataSourceBuilder.create().type(HikariDataSource.class).build();
+//  }
+//}
